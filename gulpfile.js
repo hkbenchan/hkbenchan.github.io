e116daa807698f727ff09bee9c509bed1952e1e4
ustbenchan <ustbenchan@gmail.com>
2016-08-28 20:52:02 +0800

    resized image and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-'use strict';
+// 'use strict';
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var imgcompress = require('gulp-imagemin');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
@@ -12,3 +14,21 @@ gulp.task('sass', function () {
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
+
+gulp.task('img:compress', function () {
+  return gulp.src('./img/**/*')
+    .pipe(imgcompress())
+    .pipe(gulp.dest('./dst/img'))
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('./dst/css'));
+});
+
+gulp.task('dist',
+          [ 'sass',
+            'img:compress', 
+            'minify-css' ]
+          );
